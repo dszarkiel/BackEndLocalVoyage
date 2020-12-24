5de681a9e3261985023e30f88c247be0877743c6
sampassarelli <passarelli.sam@gmail.com>
2020-12-24 11:15:22 -0600

    added users index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
 
+    def index
+        users = User.all
+        render json: users, except: [:created_at, :updated_at]
+    end
+
     def show
         user = User.find_by_id(params[:id])
         render json: user.to_json(include: [:destinations], except: [:created_at, :updated_at])
