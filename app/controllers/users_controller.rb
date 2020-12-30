7dc515a0a39209ab4e1a7417b1c6f43ec57cafec
https://learn.co/DenaNeuman <denaweiss5@gmail.com>
2020-12-30 11:32:38 -0600

    dena updates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 
+    def index
+        users = User.all 
+        render json: users 
+    end
     def show
         user = User.find_by_id(params[:id])
         render json: user.to_json(include: [:destinations], except: [:created_at, :updated_at])
