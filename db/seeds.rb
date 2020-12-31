8b06436c7ffc198e180a1b2a7c3c8a81fa5ead55
sampassarelli <passarelli.sam@gmail.com>
2020-12-30 20:35:22 -0600

    new branch initial commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ user1 = User.create(first_name: "Dom", last_name: "Szarkiel", email: "d")
 user2 = User.create(first_name: "Sam", last_name: "Passarelli", email: "sam@gmail.com")
 user3 = User.create(first_name: "Dena", last_name: "Weiss", email: "dena@gmail.com")
 
-puts "creating destination"
+puts "creating destination" 
 
 destination1 = Destination.create(name: "Sear's Tower", date_visited: "12/23/2020", address: "100 N Michigan Ave", category: "Landmark", comment: "Its SO TALL! OMG!", visited: true, cost: 50, attendees: "Mom and dad", rating: 4, user_id: user1.id, latitude: 41.8788764, longitude: -87.6359149)
 destination2 = Destination.create(name: "Bean", date_visited: "12/14/2020", address: "1234 Franklin Ave", category: "Landmark", comment: "Its SO ROUND! OMG!", visited: true, cost: 0, attendees: "Solo", rating: 2, user_id: user3.id)
