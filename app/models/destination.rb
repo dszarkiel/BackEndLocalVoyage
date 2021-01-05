efdd2ecb369be0d2bd80d9683288955a94e7a071
dszarkiel <d.szarkiel@gmail.com>
2021-01-04 20:06:12 -0600

    new dest default values set

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,3 +1,20 @@
 class Destination < ApplicationRecord
     belongs_to :user
+
+    after_initialize :set_defaults
+
+    def set_defaults
+        if self.category === ""
+            self.category = "Please enter category"
+        end
+        if self.attendees === ""
+            self.attendees = "Please enter who attended!"
+        end
+        if self.comment === ""
+            self.comment = "Please enter any thoughts you have about this place!"
+        end
+        self.cost ||= 0
+        self.rating ||= 0
+    end
+
 end
